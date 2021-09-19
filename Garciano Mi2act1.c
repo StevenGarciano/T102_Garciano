11fab8f873a81d6c70abfac7766ddf9246a0019b
Steven Kun <89185766+StevenGarciano@users.noreply.github.com>
2021-09-19 16:42:45 +0800

    Add files via upload

---
+++ Garciano Mi2act1.c
@@ -0,0 +1,12 @@
+//Garciano Mi2act1
+#include <stdio.h>
+
+int main()
+{
+    //declaration of variable
+    int age = 18;
+    //printing the variable
+    printf("I'm Steven Garciano, Age of %d years old", age);
+
+    return 0;
+}
